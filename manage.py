e2a1977bcaea659b9c847f6b05df3ca066657b74
Gopalbharti790-tech <gopalbharti231410824004@gmail.com>
2026-05-05 13:48:15 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -19,4 +19,13 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
+
+    port = os.environ.get('PORT', '8000')  # fallback to 8000
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError("Couldn't import Django") from exc
+
+    execute_from_command_line([sys.argv[0], 'runserver', f'0.0.0.0:{port}'])
